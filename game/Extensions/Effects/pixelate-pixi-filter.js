257af2c8525b4385f4413932a681e380a80d73bf
BTM <50245465+bmoorman4575@users.noreply.github.com>
2022-03-24 20:38:36 -0500

    0.0.1

---
+++ game/Extensions/Effects/pixelate-pixi-filter.js
@@ -0,0 +1,2 @@
+var gdjs;(function(a){a.PixiFiltersTools.registerFilterCreator("Pixelate",{makePIXIFilter:function(t,e){return new PIXI.filters.PixelateFilter(e.doubleParameters.size)},updatePreRender:function(t,e){},updateDoubleParameter:function(t,e,r){const i=t;e==="size"&&(i.size=r)},updateStringParameter:function(t,e,r){},updateBooleanParameter:function(t,e,r){}})})(gdjs||(gdjs={}));
+//# sourceMappingURL=pixelate-pixi-filter.js.map
